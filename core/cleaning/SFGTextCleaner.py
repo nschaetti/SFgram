dc1b8b0ae62106fd0f4979540cd021f5a2ff2ae9
Nils Schaetti <nils.schaetti@unine.ch>
2017-06-19 12:04:11 +0200

    Initial

--- core/cleaning/SFGTextCleaner.py
+++ core/cleaning/SFGTextCleaner.py
@@ -24,91 +24,34 @@ class SFGTextCleaner(SFGCleaner):
         :param text: Text to clean.
         :return: Dictionary with text and information.
         """
-        print(self.get_title(text))
-        print(self.get_author(text))
-        print("")
-    # end __call__
+        # Check if the default marker is here
+        if "**** START OF THIS PROJECT GUTENBERG" in text.upper():
+            # Split by lines
+            lines = text.split('\n')
 
-    # Get regex value of first group
-    def _get_regex_value(self, pattern, text):
-        """
-        Get regex value of first group
-        :param pattern:
-        :param text:
-        :return:
-        """
-        match = re.search(pattern, text, flags=re.MULTILINE)
-        if match is not None:
-            return match.groups(0)[0]
+            # For each line
+            result = list()
+            save = False
+            for line in lines:
+                # Check if we passe the end ebbok tag
+                if "*** END OF THIS PROJECT GUTENBERG" in line.upper():
+                    save = False
+                # end if
+
+                # Save line
+                if save:
+                    result.append(line)
+                # end if
+
+                # Check if we pass the start ebook tag
+                if "*** START OF THIS PROJECT GUTENBERG" in line.upper():
+                    save = True
+                # end if
+            # end for
+            return result.join('\n')
+        else:
+            return text
         # end if
-        return None
-    # end get_regex_value
-
-    # Get document's ID
-    def get_id(self, text):
-        """
-        Get document's id
-        :param text:
-        :return:
-        """
-        title = ""
-
-        # Possible regex
-        regex = [r"^Title: ([\w \.:'\-\"]+)", r"^The Project Gutenberg EBook of ([\w \.:'\-\"]+)",
-                 r"^Project Gutenberg Etext of ([\w \.:'\-\"]+)", r"^Project Gutenberg Etext:  ([\w \.:'\-\"]+)",
-                 r"^The Project Gutenberg Etext of ([\w \.:'\-\"]+)"]
-
-        # For each regex
-        for r in regex:
-            title_field = self._get_regex_value(r, text)
-            if title_field is not None:
-                return title_field
-            # end if
-        # end for
-    # end get_title
-
-    # Get document's title
-    def get_title(self, text):
-        """
-        Get document's title
-        :param text:
-        :return:
-        """
-        title = ""
-
-        # Possible regex
-        regex = [r"^Title: ([\w \.:'\-\"]+)", r"^The Project Gutenberg EBook of ([\w \.:'\-\"]+)",
-                 r"^Project Gutenberg Etext of ([\w \.:'\-\"]+)", r"^Project Gutenberg Etext:  ([\w \.:'\-\"]+)",
-                 r"^The Project Gutenberg Etext of ([\w \.:'\-\"]+)"]
-
-        # For each regex
-        for r in regex:
-            title_field = self._get_regex_value(r, text)
-            if title_field is not None:
-                return title_field
-            # end if
-        # end for
-    # end get_title
-
-    # Get document's author
-    def get_author(self, text):
-        """
-        Get document's author
-        :param text:
-        :return:
-        """
-        author = ""
-
-        # Possible regex
-        regex = [r"^Author: ([\w \.']+)", r"by ([\w \.']+)"]
-
-        # For each regex
-        for r in regex:
-            author_field = self._get_regex_value(r, text)
-            if author_field is not None:
-                return author_field
-            # end if
-        # end for
-    # end get_author
+    # end __call__
 
 # SFGFileCleaner
