b1de369a84b38442ad87ac7d32a321e6222a0ae6
Nils Schaetti <nils.schaetti@unine.ch>
2017-07-31 12:53:44 +0200

    Initial

---
+++ book_add_books_to_authors.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+
+import argparse
+import logging
+from mongoengine import *
+import download as dw
+from db.Author import Author
+from db.Country import Country
+from db.Book import Book
+
+######################################################
+#
+# Functions
+#
+######################################################
+
+######################################################
+#
+# Main
+#
+######################################################
+
+if __name__ == "__main__":
+
+    # Argument parser
+    parser = argparse.ArgumentParser(description="SFgram - Check all books with unknown country")
+
+    # Argument
+    parser.add_argument("--database", type=str, help="Database name", default="sfgram", required=True)
+    parser.add_argument("--log-level", type=int, help="Log level", default=20)
+    args = parser.parse_args()
+
+    # Logs
+    logging.basicConfig(level=args.log_level)
+    logger = logging.getLogger(name="SFgram")
+
+    # Connection to MongoDB
+    connect(args.database)
+
+    # For each authors
+    for author in Author.objects():
+        # Print author
+        print(author.name)
+    # end for
+
+# end if
