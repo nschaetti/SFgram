a4b0785cf0f2d58d86806d3e35dfe8dc2c1a966a
Nils Schaetti <nils.schaetti@unine.ch>
2017-08-14 17:12:38 +0200

    Initial

--- update_book_dataset.py
+++ update_book_dataset.py
@@ -14,43 +14,6 @@ import gutenberg as gb
 #######################################################
 
 
-# Create author
-def create_author(author_name):
-    """
-    Create author
-    :param author_name:
-    :return:
-    """
-    # Info
-    author_infos = wp.WikipediaBookInformation.get_author_information(author_name)
-
-    # Author object
-    author = ds.Author()
-    author.name = author_name
-
-    # Infos
-    if author_infos['found']:
-        if 'bio' in author_infos.keys():
-            author.bio = author_infos['bio']
-        # end if
-        if 'born' in author_infos.keys():
-            author.birth_date = author_infos['born']
-        # end if
-        if 'died' in author_infos.keys():
-            author.death_date = author_infos['died']
-        # end if
-        author.summary = author_infos['summary']
-        author.wikipedia['found'] = True
-        author.wikipedia['url'] = author_infos['url']
-        author.wikipedia['ambiguation'] = author_infos['ambiguation'] if 'ambiguation' in author_infos.keys() else False
-    # end if
-
-    # Add to book collection
-    author = book_collection.add(author)
-
-    return author
-# end if
-
 ######################################################
 # Main
 ######################################################
@@ -82,6 +45,20 @@ if __name__ == "__main__":
         book_collection = ds.BookCollection()
     # end if
 
+    # Load or create country collection
+    if os.path.exists(os.path.join(args.output_dir, "country.json")):
+        country_collection = ds.CountryCollection.load(os.path.join(args.output_dir, "year.json"))
+    else:
+        country_collection = ds.CountryCollection()
+    # end if
+
+    # Load or create year collection
+    if os.path.exists(os.path.join(args.output_dir, "year.json")):
+        year_collection = ds.YearCollection.load(os.path.join(args.output_dir, "year.json"))
+    else:
+        year_collection = ds.YearCollection()
+        # end if
+
     # Open category
     gutenberg_con = gb.GutenbergBookshelf()
     gutenberg_con.open(num=68, start_index=args.start_index, skip_book=args.skip_book)
@@ -91,81 +68,56 @@ if __name__ == "__main__":
         # New book
         book = ds.Book()
 
-        # Gutenberg properties
-        book.gutenberg['num'] = book_informations['#']
-        book.title = book_informations['title']
-        book.language = book_informations['language']
-        book.loc_class = book_informations['loc-class']
-        book.category = book_informations['category']
-        book.release_date = book_informations['release-date']
-        book.copyright = book_informations['copyright']
-        book.images_urls = book_informations['images-urls']
-        book.cover_art_url = book_informations['cover-url'] if 'cover-url' in book_informations.keys() else None
-        book.content_available = book_informations['content-available']
+        # Wikipedia & Goodreads information
+        wikipedia_info = wp.WikipediaBookInformation.get_book_information(book_informations['title'],
+                                                                          book_informations['authors'][0])
+        goodreads_info = gr.GoodReadsConnector.get_book_info(book_informations['title'])
+
+        # Import data
+        book.import_from_dict(book_informations, exclude=['authors', 'images', 'cover', 'content', 'genres'])
+        book.import_from_dict(wikipedia_info, exclude=['images', 'country'])
+        book.import_from_dict(goodreads_info, exclude=['small_image'])
 
         # For each authors
-        for author in book_informations['authors']:
-            author_obj = create_author(author)
-            if author_obj.id not in book.authors and author_obj.id != book.author:
-                book.authors += [author_obj.id]
+        for author_name in book_informations['authors']:
+            # Author object
+            author = ds.Author()
+
+            # Info
+            author.import_from_dict(wp.WikipediaBookInformation.get_author_information(author_name))
+
+            # Add to book collection
+            author = book_collection.add(author)
+
+            # Add to book's authors
+            if author.id not in book.authors and author.id != book.author:
+                book.authors += [author.id]
+                book.n_authors += 1
             # end if
-            if book.id not in author_obj.books:
-                author_obj.books += [book.id]
-                author_obj.n_books += 1
+
+            # Add book to author's books
+            if book.id not in author.books:
+                author.books += [book.id]
+                author.n_books += 1
             # end if
         # end for
         book.author = book.authors[0]
 
-        # Wikipedia information
-        wikipedia_info = wp.WikipediaBookInformation.get_book_information(book_informations['title'],
-                                                                          book_informations['authors'][0])
-
-        # Wikipedia properties
-        book.wikipedia['found'] = wikipedia_info['found']
-        if wikipedia_info['found']:
-            book.wikipedia['ambiguation'] = True if 'ambiguation' in wikipedia_info.keys() else False
-            book.original_title = wikipedia_info['original-title']
-            if 'plot' in wikipedia_info.keys():
-                book.plot = wikipedia_info['plot']
-            # end if
-            book.summary = wikipedia_info['summary']
-            book.wikipedia['url'] = wikipedia_info['url']
-            if 'cover-artist' in wikipedia_info.keys():
-                book.cover_artist = wikipedia_info['cover-artist']
-            # end if
-            if 'publication-year' in wikipedia_info.keys():
-                book.wikipedia['year'] = wikipedia_info['publication-year']
-            # end if
-            if 'publisher' in wikipedia_info.keys():
-                book.publisher = wikipedia_info['publisher']
-            # end if
-            if 'published-in' in wikipedia_info.keys():
-                book.published_in = wikipedia_info['published-in']
-            # end if
+        # Country
+        if 'country' in wikipedia_info.keys():
+            country = ds.Country(wikipedia_info['country'])
+            country_collection.add(country)
+            country.books.append(book_collection.get_next_book_id())
+            country.n_books += 1
+            book.country = country.id
         # end if
 
-        # Goodreads information
-        goodreads_info = gr.GoodReadsConnector.get_book_info(book_informations['title'])
-
-        # Goodreads properties
-        book.goodreads['found'] = goodreads_info['found']
-        if goodreads_info['found']:
-            book.ISBN13 = goodreads_info['isbn13']
-            book.ISBN = goodreads_info['isbn']
-            book.similar_books = goodreads_info['similar-books']
-            book.goodreads['url'] = goodreads_info['url']
-            book.description = goodreads_info['description']
-            book.average_rating = goodreads_info['average-rating']
-            book.language_code = goodreads_info['language-code']
-            book.rating_count = goodreads_info['rating-count']
-            if 'pages' in goodreads_info.keys():
-                book.pages = goodreads_info['pages']
-            # end if
-            book.format = goodreads_info['format']
-            if 'publication-year' in goodreads_info.keys():
-                book.goodreads['year'] = goodreads_info['publication-year']
-            # end if
-        # end if
+        # Year
+        pub_year = book.wikipedia['year'] if book.wikipedia['year'] != -1 else book.goodreads['year']
+        year = ds.Year(year=pub_year)
+        year.books.append(book.id)
+        year.n_books += 1
+        year_collection.add(year)
 
         # Save gutenberg images
         for (data, name) in book_informations['images']:
@@ -173,7 +125,7 @@ if __name__ == "__main__":
         # end for
 
         # Save gutenberg images
-        if wikipedia_info['found']:
+        if wikipedia_info['wikipedia']['found']:
             for (data, name) in wikipedia_info['images']:
                 book_collection.save_image(dataset.get_dataset_directory(), book_collection.get_next_book_id(), data,
                                            name)
@@ -187,9 +139,9 @@ if __name__ == "__main__":
         # end if
 
         # Save small image
-        if 'small-image' in goodreads_info.keys():
+        if 'small_image' in goodreads_info.keys():
             book_collection.save_image(dataset.get_dataset_directory(), book_collection.get_next_book_id(),
-                                           goodreads_info['small-image'][0], goodreads_info['small-image'][1])
+                                           goodreads_info['small_image'][0], goodreads_info['small_image'][1])
             # end if
 
         # Save content
@@ -201,8 +153,9 @@ if __name__ == "__main__":
         # Add to book collection
         book_collection.add(book)
 
-        # Save book collection
+        # Save collections
         book_collection.save(dataset.get_dataset_directory())
+        country_collection.save(dataset.get_dataset_directory())
     # end for
 
 # end if
