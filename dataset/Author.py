a7aa034e3cac5d113614ab209e5e05500779624f
Nils Schaetti <nils.schaetti@unine.ch>
2017-08-14 17:11:51 +0200

    Initial

--- dataset/Author.py
+++ dataset/Author.py
@@ -29,4 +29,31 @@ class Author(object):
         self.books = list()
     # end __init__
 
+    ############################################
+    # Public
+    ############################################
+
+    # Import properties from dictionary
+    def import_from_dict(self, dict_var, exclude=[]):
+        """
+        Import properties from dictionary
+        :param dict_var: Dictionary to import
+        :param exclude: Key to exclude
+        """
+        # For each dict's key
+        for key in dict_var.keys():
+            if key not in exclude:
+                setattr(self, key, dict_var[key])
+            # end if
+        # end for
+    # end import_from_dict
+
+    ############################################
+    # Private
+    ############################################
+
+    ############################################
+    # Static
+    ############################################
+
 # end Book
