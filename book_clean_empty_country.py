c97bf19b58de6e30042192a215f74adc423230a2
nschaetti <n.schaetti@gmail.com>
2017-09-29 20:04:25 +0200

    Book clean empty country

---
+++ book_clean_empty_country.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#
+
+import argparse
+import logging
+import os
+import wikipediaorg as wp
+import dataset as ds
+import goodreadscom as gr
+import gutenberg as gb
+from dataset.CountryCollection import CountryCollection
+from dataset.Country import Country
+
+######################################################
+# Functions
+#######################################################
+
+
+######################################################
+# Main
+######################################################
+
+if __name__ == "__main__":
+
+    # Argument parser
+    parser = argparse.ArgumentParser(description="SFgram - Update the SFGram book dataset")
+
+    # Argument
+    parser.add_argument("--dataset-dir", type=str, help="Dataset directory", required=True)
+    args = parser.parse_args()
+
+    # Dataset
+    dataset = ds.Dataset(args.dataset_dir)
+
+    # Load or create collections
+    book_collection = ds.BookCollection.create(args.dataset_dir)
+    country_collection = ds.CountryCollection.create(args.dataset_dir)
+    year_collection = ds.YearCollection.create(args.dataset_dir)
+
+    # For each country
+    for country in country_collection.get_countries():
+        if len(country.authors) == 0:
+            print(country.name)
+            country_collection.remove(country)
+        # end if
+    # end for
+
+    # Save collections
+    book_collection.save(dataset.get_dataset_directory())
+    country_collection.save(dataset.get_dataset_directory())
+    year_collection.save(dataset.get_dataset_directory())
+
+# end if
