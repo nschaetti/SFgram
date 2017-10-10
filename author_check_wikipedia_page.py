0a49a9bd4a33503a5f8467116d57a95d4bf3747b
Nils Schaetti <nils.schaetti@unine.ch>
2017-10-10 17:13:07 +0200

    Wikipedia page checking for author

--- author_check_wikipedia_page.py
+++ author_check_wikipedia_page.py
@@ -36,13 +36,13 @@ if __name__ == "__main__":
     book_collection = ds.BookCollection.create(args.dataset_dir)
 
     # Author attributes
-    author_attrs = ['bord', 'died', 'summary', 'bio']
+    author_attrs = ['born', 'died', 'summary', 'bio']
 
     # For each author
     for author in book_collection.get_authors():
         if author.id >= args.start:
             # Show
-            print(u"Author {} name {} date {}".format(author.id, author.name, author.birth_date))
+            print(u"Author {} name {} date {}/{}".format(author.id, author.name, author.born, author.died))
             if 'url' in author.wikipedia:
                 print(u"Wikipedia page : {}".format(author.wikipedia['url']))
             else:
@@ -72,10 +72,17 @@ if __name__ == "__main__":
             elif page_answer == u"":
                 pass
             else:
+                # Searches
+                author_research = \
+                    [
+                        author.name,
+                        page_answer[page_answer.rfind(u'/')+1:].replace(u'_', u' ').replace(u'(', u'').replace(u')', u''),
+                        page_answer[page_answer.rfind(u'/')+1:].replace(u'_', u' ')
+                    ]
+
                 # Wikipedia informations
-                wikipedia_info = wp.WikipediaBookInformation.get_author_information_from_url(author.name, page_answer)
-                print(wikipedia_info)
-                exit()
+                wikipedia_info = wp.WikipediaBookInformation.get_author_information_from_url(author_research, page_answer)
+
                 # Wikipedia part
                 author.wikipedia['url'] = page_answer
                 author.wikipedia['found'] = True
@@ -85,7 +92,11 @@ if __name__ == "__main__":
                     try:
                         setattr(author, attr, wikipedia_info[attr])
                     except KeyError:
-                        pass
+                        try:
+                            delattr(author, attr)
+                        except AttributeError:
+                            pass
+                            # end try
                     # end try
                 # end for
             # end if
